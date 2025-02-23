4494bd0175a81ffd62520e3cd101fead540df38f
ChristianHooper <ChristianHooper@protonmail.com>
2025-02-22 18:36:07 -0800

    Finished Wilson CPG model for flight.

--- chapter_one/habituationGWR.py
+++ chapter_one/habituationGWR.py
@@ -9,11 +9,11 @@ def run():
 
     time = 30
     x = np.zeros(time); x[2::5] = 1 # Pre-synaptic input overtime
-    y = np.zeros(time) # Motoneuron output 
+    y = np.zeros(time) # Motoneuron output
     v = 4 # Axonal weight
     d = 0.7 # Axonal weight damping due to repeated stimulation
 
-    for n in range(time): # Calculates neuron response to incoming sensory afferents 
+    for n in range(time): # Calculates neuron response to incoming sensory afferents
         y[n] = v * x[n] # $f(x)=vx(t)$ LaTeX
         if x[n] == 1: v = v * d # Decreases post-synaptic response from continued stimulation
 
@@ -24,10 +24,10 @@ def run():
 
     axs[1].set_title("Post-synaptic input")
     axs[1].plot(y)
-    
+
     plt.tight_layout(pad=1)
     plt.show()
-    
+
 
 if __name__ == "__main__":
     run()
--- chapter_two/wilsonCPG.py
+++ chapter_two/wilsonCPG.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''
+Script implements a linear version of Wilson's model of the locust-flight central pattern generator oscillation indicates fatigue.
+'''
 
 time = 100
 fly = 10 # Sets time to begin flight oscillations
 
 y = np.zeros((4,time)) # Neural states
 x = np.zeros((1,time)); x[0,fly]=1 # Pre-synaptic input
-
-print(x)
 
 w = np.array([ # Post-synaptic weights
     [0.9,    0.2,  0,    0],
@@ -18,13 +19,11 @@ w = np.array([ # Post-synaptic weights
 
 v = np.array([0, 1, 0, 0]) # Pre-synaptic weights
 
-for t in range(time): y[:,t] = w @ y[:,t-1] + v * x[:,t-t]
+for t in range(time): y[:,t] = w @ y[:,t-1] + v * x[:,t-1] # Neural interaction
 
-#print(y)
-
-plt.plot(np.arange(time), y[0,:])
-plt.plot(np.arange(time), y[1,:])
-plt.plot(np.arange(time), y[2,:])
-plt.plot(np.arange(time), y[3,:])
+plt.plot(np.arange(time), y[0,:], ':', label='Recurrent Leak-Integrator y_4')
+plt.plot(np.arange(time), y[1,:], label='Oscillation Motor Inhibitor y_2')
+plt.plot(np.arange(time), y[2,:], label='Oscillation Motor Inhibitor y_3')
+plt.plot(np.arange(time), y[3,:], ':', label='Recurrent Leak-Integrator y_4')
+plt.legend(title='Neurons Function')
 plt.show()
-
