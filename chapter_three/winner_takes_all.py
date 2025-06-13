076bf3568fee271f28f2eee14fe3d95816deecc2
ChristianHooper <ChristianHooper@protonmail.com>
2025-06-12 22:10:27 -0700

    Created laminate function and winner weight matrix.

--- chapter_three/winner_takes_all.py
+++ chapter_three/winner_takes_all.py
@@ -2,31 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+# Takes a matrix size, and a key to diagonally impresses onto a matrix
+def laminate(size=8, key=[-1,1,-1]): # Key must be an old length
+    mm = (len(key)-1)/2 # Half size of key for insertion
+    key_pattern = np.arange(-mm, mm+1)
+    base = np.zeros((size,size))
+
+    for n in range(len(base)):
+        for i, offset in enumerate(key_pattern):
+            # Impresses patterns key and loops edges
+            base[n, int((n + offset)%len(base))] = key[i]
+    return np.array(base)
+
+
 # Gaussian function: $G(x,\sigma)=\frac{1}{\sqrt{2\pi\sigma}}e^{-\frac{x^2}{2\sigma^2}}$
-gaussian = lambda s,x: np.exp(-(x**2/(2 * s**2)))
-ranges = 11
-test = np.zeros(ranges); test[5] = 1
-#light = np.ones(ranges); light[45:55] = 3
-#print(light)
+# np.exp(-0.5 * ((x/sd)**2))
+gaussian = lambda sd, x: np.exp(-(x**2/(2 * sd**2)))
+ranges = 51
+
+#light = np.ones(ranges); light[int(ranges*0.33):int(ranges*0.66)] = 3
+
 length = [int(-ranges/2),math.ceil(ranges/2)]
-weight_matrix = np.eye(len(range(*length))) # Identity matrix
-sd = [0.75, 1.5] # Standard deviation
-ap = [0.1, 0.3] # Amplitude
 
-g_one = np.array([int(gaussian(sd[0], n)* 10) for n in range(*length)])
-g_two = np.array([int(gaussian(sd[1], n)* 10) for n in range(*length)]) * 0.5
+sd = [15, 3] # Standard deviation
+ap = [0.3, 1.0] # Amplitude
 
-dog = (g_one - g_two)
+g_one = np.array([gaussian(sd[0], n,) for n in range(*length)]) * ap[0]
+g_two = np.array([gaussian(sd[1], n,) for n in range(*length)]) * ap[1]
 
-print(test)
-for n in range(len(test)): print(test[5-n:10-n], test[10-n:5-n])
+dog = (g_two - g_one)
+dog_3d = laminate(ranges, dog)
+#wave = dog_3d @ light
 
-
-
-#recurrent_weight = np.array()
-
-
-# TODO: Laminating function, need to work more efficiency
 
 # Plotting
 fig = plt.figure(figsize=(8,3))
@@ -34,12 +41,13 @@ fig = plt.figure(figsize=(8,3))
 ax_2d = fig.add_subplot(1,2,1, title='Light & Response')
 ax_2d.plot(np.arange(ranges), dog)
 
+
 x_axis = np.linspace(1, ranges, ranges)
 y_axis = np.linspace(1, ranges, ranges)
 x_grid, y_grid = np.meshgrid(x_axis, y_axis)
 
 ax_3d = fig.add_subplot(1,2,2, projection='3d')
-#ax_3d.plot_surface(x_grid, y_grid, dog_3d, cmap='twilight')
+ax_3d.plot_surface(x_grid, y_grid, dog_3d, cmap='twilight')
 
 
 plt.show()
