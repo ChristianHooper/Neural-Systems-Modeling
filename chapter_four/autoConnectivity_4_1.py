da21a9180c723fd769d2cea1c271e89218db3f12
ChristianHooper <ChristianHooper@protonmail.com>
2025-09-07 20:32:56 -0700

    Continued work on auto-associator networks for memory.

--- chapter_four/autoConnectivity_4_1.py
+++ chapter_four/autoConnectivity_4_1.py
@@ -3,15 +3,16 @@ import matplotlib.pyplot as plt
 from ch4_util import *
 
 p = np.array(
-    [[1,1,1,1,1,0,0,0,0,0],
-    [0,0,0,0,0,1,1,1,1,1]]
+    [[1,1,1,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,1,1,1]]
     )
 
 ex = np.array([[1,1,0], [0,0,1]])
 
-#print(hebb_matrix(p))
-#print(post_synaptic_matrix(p))
-#print(pre_synaptic_matrix(p))
-print(Hopfield_matrix(ex))
+print(hebb_matrix(p),'\n')
+print(post_synaptic_matrix(p),'\n')
+print(pre_synaptic_matrix(p),'\n')
+print(Hopfield_matrix(p))
+
 
 neural_state = np.array([.4, .3, .5, .3, .4, .2, .1, .1, .2, .1])
